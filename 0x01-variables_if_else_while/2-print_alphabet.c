b12f8149a196e60a747ecb716bb60abc490a1dd8
ahmedha-ssan <ammodi9@gmail.com>
2023-03-17 13:50:55 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,13 @@
  */
 int main(void)
 {
-	char c = 'a';
-       for(c;c<='z';c++){putchar(c);}
+	char c;
+    for(c = 'a';c<='z';c++)
+    {
+        putchar(c);
+    }
 	putchar('\n');
 	return (0);
 }
+
+
