1b9173cb6094adbd04d8e19a829fa23832bbed95
ahmedha-ssan <ammodi9@gmail.com>
2023-03-21 12:41:51 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
+/*
+ * File: -alphabet.c
+ * Auther: Ahmed Hassan
+ */
+
 #include "main.h"
+
 /**
- * main - Print _putchar
- * author AhmedHassan
- * Return: Always o
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *s print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter);
-_putchar('\n');
-return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
