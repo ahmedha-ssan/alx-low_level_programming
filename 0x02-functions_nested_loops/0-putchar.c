1ec5021a68ccfdb9de03b25017a127077c561808
ahmedha-ssan <ammodi9@gmail.com>
2023-03-21 11:04:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include <stdlib.h>
+#include "main.h"
 /**
- * main 
- * prints the string "Holberton".
- * Return: 0 .
+ * main - print _putchar
+ * author AhmedHassan
+ * return 0
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-
